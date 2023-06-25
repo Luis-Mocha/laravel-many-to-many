da59a1f1fc1c187c14737edb3b1128fc2052b24d
Luis-Mocha <luismocha97@gmail.com>
2023-06-25 23:20:47 +0200

    migration technologies and pivot table

--- database/migrations/2023_06_25_211338_create_project_technology_table.php
+++ database/migrations/2023_06_25_211338_create_project_technology_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('technologies', function (Blueprint $table) {
+        Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
-
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('technologies');
+        Schema::dropIfExists('project_technology');
     }
 };
